f5933ca035297b226dc011f38c8b69f2ad5761e9
Ben Smith <ben.smith@wmplus.co.uk>
2025-11-27 15:23:43 +0000

    Adjust chatbot button styling for consistent display order
    - Added `nw-z-10` to ensure proper stacking context and resolve potential overlap issues.

--- resources/views/livewire/chatbot.blade.php
+++ resources/views/livewire/chatbot.blade.php
@@ -2,7 +2,7 @@
     <div x-data="{ show: @entangle('show').live }" class="nw-z-40">
         <div x-cloak
              x-show="!show"
-             class="nw-fixed nw-bottom-5 nw-right-5 nw-fill-green-500">
+             class="nw-fixed nw-bottom-5 nw-right-5 nw-z-10 nw-fill-green-500">
             <button @click="show = true"
                     type="button"
                     class="nw-inline-flex nw-items-center nw-gap-x-2 nw-rounded-md {{ config('chattera.ui.button.background', 'nw-bg-indigo-600') }} nw-px-3.5 nw-py-2.5 nw-text-sm nw-font-semibold {{ config('chattera.ui.button.text_color', 'nw-text-white') }} nw-shadow-sm hover:{{ config('chattera.ui.button.hover', 'nw-bg-indigo-500') }} focus-visible:nw-outline focus-visible:nw-outline-2 focus-visible:nw-outline-offset-2 focus-visible:nw-outline-indigo-600">
